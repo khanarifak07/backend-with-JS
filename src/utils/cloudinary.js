7c0ba166c45a6b1ee65f2373e51988bc93a2b367
Arif Khan <khanarifak07@gmail.com>
2023-12-20 16:18:28 +0530

    cloudinary and multer function created

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const UploadFilesOnCloudinary = function (localFilePath) {
+  try {
+    if (!localFilePath) return null;
+    //upload file on cloudinary
+    const response = cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File successfully uploaded on cloudinary", response.url); // here we can get url to store in the database
+    console.log(response);
+    fs.unlinkSync(localFilePath); // to remove the loclally saved temo files after successful uploading of image
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { UploadFilesOnCloudinary };
