0822e6278d91b0cc430df80a986b99f912b9128a
Arif Khan <khanarifak07@gmail.com>
2024-01-17 19:43:32 +0530

    routes imported and added in app.use

--- src/app.js
+++ src/app.js
@@ -49,8 +49,24 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 //import router
+import commentRouter from "./routes/comment.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/videos", videoRouter);
 
 export { app };
